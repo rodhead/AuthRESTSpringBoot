8e0356e27cc585ec6f896ae7798e18bfc467d4a3
rodHead <misbahrahman7300@yahoo.com>
2018-03-20 00:32:14 +0530

    [20/03/2018] FetchAccount class commit against the changes made to close the hibernate connection

--- src/main/java/com/auth/api/dao/FetchAccount.java
+++ src/main/java/com/auth/api/dao/FetchAccount.java
@@ -3,6 +3,7 @@ package com.auth.api.dao;
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
@@ -11,19 +12,34 @@ import com.auth.api.model.AccountDetails;
 
 @Configuration
 public class FetchAccount {
-	
-	@Autowired App app;
-	
-	
-	public List<AccountDetails> fetch(){
-		SessionFactory sf=app.runn();
-		Session session=sf.openSession();
-		session.beginTransaction();
-		List<AccountDetails> list=new ArrayList<AccountDetails>();
-		list=session.createSQLQuery("select * from AccountDetails").list();
-		
+
+	@Autowired
+	App app;
+
+	Session session = null;
+	SessionFactory sf = null;
+
+	@SuppressWarnings("unchecked")
+	public List<AccountDetails> fetch() {
+		List<AccountDetails> list = new ArrayList<AccountDetails>();
+		try {
+			sf = app.runn();
+			session = sf.openSession();
+			session.beginTransaction();
+
+			Query query = session.createQuery("from AccountDetails");
+			list = query.list();
+
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+
+		finally {
+			session.close();
+			sf.close();
+		}
+
 		return list;
-		
 	}
 
 }
